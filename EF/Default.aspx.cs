4fb929e1c094fa91898c7b0008acc28d41b1d9c8
Per A. Bergersen <pab2222@hotmail.com>
2023-05-03 23:14:06 +0200

    Add project files.

---
+++ EF/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EF
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //snippet for å vise insert. dette kan legges i en dblayer som metoder
+            var db=new PersonsEntities();
+            var person=new Person();
+            person.Id = 2;
+            person.PostalCode = 1630;
+            person.FirstName = "Nico";
+            person.LastName = "Luring";
+            db.Person.Add(person);
+            db.SaveChanges();
+        }
+    }
+}
+
+//database script
